48617dc040a11093e3b437b8dd26a85c8ab36454
Svoykina N <nadya.svojkina@gmail.com>
2017-10-20 14:42:19 +0300

    Changed variables to ES6, deleted unused

--- source/lab2.js
+++ source/lab2.js
@@ -4,11 +4,11 @@
 
 function saveTextAsFile()
 {
-    var textToWrite = document.getElementById("inputTextToSave").value;
-    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-    var fileNameToSaveAs ="123";// document.getElementById("inputFileNameToSaveAs").value;
+    const textToWrite = document.getElementById("inputTextToSave").value;
+    const textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    const fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
 
-    var downloadLink = document.createElement("a");
+    const downloadLink = document.createElement("a");
     downloadLink.download = fileNameToSaveAs;
     downloadLink.innerHTML = "Download File";
     if (window.webkitURL != null)
@@ -26,19 +26,14 @@ function saveTextAsFile()
     downloadLink.click();
 }
 
-function destroyClickedElement(event)
-{
-    document.body.removeChild(event.target);
-}
-
-var canvas = document.getElementById("lab02");
-var canvasWidth = canvas.width;
-var canvasHeight = canvas.height;
-var ctx = canvas.getContext("2d");
-var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+const canvas = document.getElementById("lab02");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const ctx = canvas.getContext("2d");
+const canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
 function drawPixel (x, y, r, g, b, a) {
-    var index = (x + y * canvasWidth) * 4;
+    let index = (x + y * canvasWidth) * 4;
 
     canvasData.data[index + 0] = r;
     canvasData.data[index + 1] = g;
@@ -49,19 +44,19 @@ function drawPixel (x, y, r, g, b, a) {
 function loadFileAsText()
 {
     const canvas = document.getElementById('lab02');
-    const ctx = canvas.getContext('2d');
+    let ctx = canvas.getContext('2d');
     ctx.fillStyle = "#ffd95a";
     ctx.fillRect(0, 0, 100, 100);
     const log = "/n";
 
-    var fileToLoad = document.getElementById("fileToLoad").files[0];
+    const fileToLoad = document.getElementById("fileToLoad").files[0];
 
-    var fileReader = new FileReader();
-    fileReader.onload = function(fileLoadedEvent)
+    const fileReader = new FileReader();
+    fileReader.addEventListener('load', fileLoadedEvent =>
     {
-        var textFromFileLoaded = fileLoadedEvent.target.result;
-        for(var i = 0; i <= 10; i++) {
-            for (var j = 0; j <= 10; j++) {
+        let textFromFileLoaded = fileLoadedEvent.target.result;
+        for(let i = 0; i <= 10; i++) {
+            for (let j = 0; j <= 10; j++) {
                 if(textFromFileLoaded[i + j*10] == 1) {
                     ctx.fillStyle = "#000000";
                     ctx.fillRect(i*10, j*10, 9, 9);
@@ -69,21 +64,6 @@ function loadFileAsText()
             }
         }
         document.getElementById("inputTextToSave").value = textFromFileLoaded;
-    };
+    });
     fileReader.readAsText(fileToLoad, "UTF-8");
-}
-
-function loadFileAsImage(event)
-{
-    var selectedFile = event.target.files[0];
-    var reader = new FileReader();
-
-    var imgtag = document.getElementById("image");
-    imgtag.title = selectedFile.name;
-
-    reader.onload = function(event) {
-        imgtag.src = event.target.result;
-    };
-
-    reader.readAsDataURL(selectedFile);
 }
